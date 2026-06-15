aa722d38d606996211b8315a14441fdb45d0560d
Emmanuel Marmolejo Rea <emmanuel.marmolejo@tamex.mx>
2026-06-15 14:50:21 -0600

    Deploy API Render

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import sys
-import os
-
-# permitir que Python encuentre "src"
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-
-from src.data.conexion_sql import obtener_datos
+import requests
+import pandas as pd
 
 st.set_page_config(
     page_title="Reporte de Márgenes",
@@ -16,8 +11,33 @@ st.set_page_config(
 st.title("📊 Margen de Utilidad en Pedidos Afectados")
 st.write("Reportes Área de Precios")
 
-# Cargar datos
-df = obtener_datos()
+# ---------------------------
+# URL DE LA API
+# ---------------------------
+API_URL = "http://127.0.0.1:8000/datos"
 
-st.subheader("Datos desde SQL Server")
+# ---------------------------
+# FUNCIÓN API
+# ---------------------------
+def obtener_datos_api():
+    try:
+        response = requests.get(API_URL, timeout=30)
+
+        if response.status_code != 200:
+            st.error(f"Error conectando con la API: {response.status_code}")
+            return pd.DataFrame()
+
+        data = response.json()
+        return pd.DataFrame(data)
+
+    except Exception as e:
+        st.error(f"Error de conexión: {e}")
+        return pd.DataFrame()
+
+# ---------------------------
+# CARGA DE DATOS
+# ---------------------------
+df = obtener_datos_api()
+
+st.subheader("Datos desde API")
 st.dataframe(df)
